75f456e971c9fffb6b5de7d3ce9e4fd3ef42019c
Naudria <rima.jean@gmail.com>
2019-04-10 17:29:19 -0500

    Start js file with a document ready callback function. Create a function called bindClickHandlers that hijacks the click event for all_beersand collect it with jquery. Prevent the default action from happening and check if everything works in the console.

--- app/assets/javascripts/beers.js
+++ app/assets/javascripts/beers.js
@@ -1,3 +1,10 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(() => {
+	bindClickHanders()
+})
+
+const bindClickHanders = () => {
+	$('.all_beers').on('click', (e) => {
+    e.preventDefault()
+    console.log("hello")
+  })
+}
